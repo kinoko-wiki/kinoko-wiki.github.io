6d871368ea49b7f7f2cda86cb31e4e22a4865f67
Shoko Kogawa <fly.child1017@gmail.com>
2025-09-20 00:16:05 +0900

    Add files via upload

---
+++ images.js
@@ -0,0 +1,23 @@
+// 画像URLとテキストのリストを定義
+const imageData = [
+    {
+        url: "images/pic01.png",
+        text: "親指姫",
+        uid: "COCO1",
+        serverId: "251"
+    },
+    {
+        url: "images/pic02.png",
+        text: "パンがないならケーキを食べればいいじゃない",
+        uid: "COCO1",
+        serverId: "251"
+
+    },
+    {
+        url: "images/pic03.png",
+        text: "コラボ企画",
+        uid: "COCO1",
+        serverId: "251"
+    },
+    // さらに画像とテキストのペアをここに追加
+];
