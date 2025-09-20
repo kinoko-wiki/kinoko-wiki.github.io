5a47c719c4eabd4bb444c357f53bdf0556eeec81
tabiru <65129551+sandeep-maplem@users.noreply.github.com>
2025-09-20 23:02:40 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,63 @@
+// 画像URLとテキストのリストを定義
+export const imageData = [
+  {
+    "url": "https://kinoko-wiki.com/easyedit/kcfinder/upload/images/%E5%A4%96%E8%A6%B3%E4%BA%A4%E6%B5%81%E4%BC%9A/202509/test01.png",
+    "text": "◯◯◯◯◯◯◯◯◯◯",
+    "uid": "TEST1",
+    "serverId": "test"
+  },
+  {
+    "url": "https://kinoko-wiki.com/easyedit/kcfinder/upload/images/%E5%A4%96%E8%A6%B3%E4%BA%A4%E6%B5%81%E4%BC%9A/202509/test02.png",
+    "text": "アラビアンタイムマシン",
+    "uid": "TEST2",
+    "serverId": "test"
+  },
+  {
+    "url": "https://kinoko-wiki.com/easyedit/kcfinder/upload/images/%E5%A4%96%E8%A6%B3%E4%BA%A4%E6%B5%81%E4%BC%9A/202509/test03.png",
+    "text": "狼の使者",
+    "uid": "TEST3",
+    "serverId": "test"
+  },
+  {
+    "url": "https://kinoko-wiki.com/easyedit/kcfinder/upload/images/%E5%A4%96%E8%A6%B3%E4%BA%A4%E6%B5%81%E4%BC%9A/202509/test04.png",
+    "text": "森の王子様",
+    "uid": "TEST4",
+    "serverId": "test"
+  },
+  {
+    "url": "https://kinoko-wiki.com/easyedit/kcfinder/upload/images/%E5%A4%96%E8%A6%B3%E4%BA%A4%E6%B5%81%E4%BC%9A/202509/test05.png",
+    "text": "宇宙魔法戦士",
+    "uid": "TEST5",
+    "serverId": "test"
+  },
+  {
+    "url": "https://kinoko-wiki.com/easyedit/kcfinder/upload/images/%E5%A4%96%E8%A6%B3%E4%BA%A4%E6%B5%81%E4%BC%9A/202509/test06.png",
+    "text": "竜を駆る者",
+    "uid": "TEST6",
+    "serverId": "test"
+  },
+  {
+    "url": "https://kinoko-wiki.com/easyedit/kcfinder/upload/images/%E5%A4%96%E8%A6%B3%E4%BA%A4%E6%B5%81%E4%BC%9A/202509/test07.png",
+    "text": "豊穣の使者",
+    "uid": "TEST7",
+    "serverId": "test"
+  },
+  {
+    "url": "https://kinoko-wiki.com/easyedit/kcfinder/upload/images/%E5%A4%96%E8%A6%B3%E4%BA%A4%E6%B5%81%E4%BC%9A/202509/test08.png",
+    "text": "裁定を下す者",
+    "uid": "TEST8",
+    "serverId": "test"
+  },
+  {
+    "url": "https://kinoko-wiki.com/easyedit/kcfinder/upload/images/%E5%A4%96%E8%A6%B3%E4%BA%A4%E6%B5%81%E4%BC%9A/202509/test09.png",
+    "text": "( ˘ω˘)ｽﾔｧ",
+    "uid": "TEST9",
+    "serverId": "test"
+  },
+  {
+    "url": "https://kinoko-wiki.com/easyedit/kcfinder/upload/images/%E5%A4%96%E8%A6%B3%E4%BA%A4%E6%B5%81%E4%BC%9A/202509/test10.png",
+    "text": "お菓子の魔法使い",
+    "uid": "TEST0",
+    "serverId": "test"
+  }
+];
