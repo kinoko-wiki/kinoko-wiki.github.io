7882a7ce243067348dcc03a3d382baa3516c1519
tabiru <65129551+sandeep-maplem@users.noreply.github.com>
2025-09-20 22:12:03 +0900

    Update images.js

--- images.js
+++ images.js
@@ -1,26 +1,66 @@
 // 画像URLとテキストのリストを定義
 const imageData = [
-    {
-        url: "images/pic01.png",
-        text: "親指姫",
-        uid: "COCO1",
-        serverId: "251"
-    },
-    {
-        url: "images/pic02.png",
-        text: "パンがないならケーキを食べればいいじゃない",
-        uid: "COCO1",
-        serverId: "251"
-    },
-    {
-        url: "images/pic03.png",
-        text: "コラボ企画",
-        uid: "COCO1",
-        serverId: "251"
-    },
-    // さらに画像とテキストのペアをここに追加
+  {
+    "url": "https://kinoko-wiki.com/easyedit/kcfinder/upload/images/%E5%A4%96%E8%A6%B3%E4%BA%A4%E6%B5%81%E4%BC%9A/202509/test01.png",
+    "text": "◯◯◯◯◯◯◯◯◯◯◯◯◯◯◯◯◯◯◯◯",
+    "uid": "TEST1",
+    "serverId": "test"
+  },
+  {
+    "url": "https://kinoko-wiki.com/easyedit/kcfinder/upload/images/%E5%A4%96%E8%A6%B3%E4%BA%A4%E6%B5%81%E4%BC%9A/202509/test02.png",
+    "text": "アラビアンタイムマシン",
+    "uid": "TEST2",
+    "serverId": "test"
+  },
+  {
+    "url": "https://kinoko-wiki.com/easyedit/kcfinder/upload/images/%E5%A4%96%E8%A6%B3%E4%BA%A4%E6%B5%81%E4%BC%9A/202509/test03.png",
+    "text": "狼の使者",
+    "uid": "TEST3",
+    "serverId": "test"
+  },
+  {
+    "url": "https://kinoko-wiki.com/easyedit/kcfinder/upload/images/%E5%A4%96%E8%A6%B3%E4%BA%A4%E6%B5%81%E4%BC%9A/202509/test04.png",
+    "text": "森の王子様",
+    "uid": "TEST4",
+    "serverId": "test"
+  },
+  {
+    "url": "https://kinoko-wiki.com/easyedit/kcfinder/upload/images/%E5%A4%96%E8%A6%B3%E4%BA%A4%E6%B5%81%E4%BC%9A/202509/test05.png",
+    "text": "宇宙魔法戦士",
+    "uid": "TEST5",
+    "serverId": "test"
+  },
+  {
+    "url": "https://kinoko-wiki.com/easyedit/kcfinder/upload/images/%E5%A4%96%E8%A6%B3%E4%BA%A4%E6%B5%81%E4%BC%9A/202509/test06.png",
+    "text": "竜を駆る者",
+    "uid": "TEST6",
+    "serverId": "test"
+  },
+  {
+    "url": "https://kinoko-wiki.com/easyedit/kcfinder/upload/images/%E5%A4%96%E8%A6%B3%E4%BA%A4%E6%B5%81%E4%BC%9A/202509/test07.png",
+    "text": "豊穣の使者",
+    "uid": "TEST7",
+    "serverId": "test"
+  },
+  {
+    "url": "https://kinoko-wiki.com/easyedit/kcfinder/upload/images/%E5%A4%96%E8%A6%B3%E4%BA%A4%E6%B5%81%E4%BC%9A/202509/test08.png",
+    "text": "裁定を下す者",
+    "uid": "TEST8",
+    "serverId": "test"
+  },
+  {
+    "url": "https://kinoko-wiki.com/easyedit/kcfinder/upload/images/%E5%A4%96%E8%A6%B3%E4%BA%A4%E6%B5%81%E4%BC%9A/202509/test09.png",
+    "text": "( ˘ω˘)ｽﾔｧ",
+    "uid": "TEST9",
+    "serverId": "test"
+  },
+  {
+    "url": "https://kinoko-wiki.com/easyedit/kcfinder/upload/images/%E5%A4%96%E8%A6%B3%E4%BA%A4%E6%B5%81%E4%BC%9A/202509/test10.png",
+    "text": "お菓子の魔法使い",
+    "uid": "TEST0",
+    "serverId": "test"
+  }
 ];
-
 // Google Apps ScriptのURL
 const scriptUrl = "https://script.google.com/macros/s/AKfycbz8tSaybeBFlyNv5KAQstxHcY8a2hfVNiU3qz6JsizcgDJSFeYrG5E_ixdrz9096eE60w/exec";
 
