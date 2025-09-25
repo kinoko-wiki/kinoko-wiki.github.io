3513b228c089cd684220054ff6a7e3616fe9982b
Shoko Okochi <fly.child1017@gmail.com>
2025-09-26 00:50:21 +0900

    update

--- data.js
+++ data.js
@@ -1145,5 +1145,11 @@ export const imageData = [
     "text": "ネギ！",
     "uid": 91904,
     "serverId": 1035
+  },
+  {
+    "url": "https://kinoko-wiki.com/easyedit/kcfinder/upload/images/%E5%A4%96%E8%A6%B3%E4%BA%A4%E6%B5%81%E4%BC%9A/202509/251_11C05.jpeg",
+    "text": "𐔌ᵔ꜆ ܸ•⩊•ᵔ 𐦯꜆",
+    "uid": "11C05",
+    "serverId": 251
   }
 ];
